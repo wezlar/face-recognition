0d070039209c4fef5845f9ad62d27f5d75ed69f5
Dean Pugh <deanpugh@deanpugh.com>
2019-05-24 08:11:14 +0100

    Inital commit once basic react app is working

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  setupFilesAfterEnv: [
+    '<rootDir>/jest.setup.js',
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+  ],
+  transform: {
+    '.*': 'babel-jest',
+    '^.+\\.js?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|svg|ttf|css)$': '<rootDir>/testHelpers/assetsTransformer.js',
+    '@jest-helpers(.*)$': '<rootDir>/testHelpers/$1',
+  },
+  collectCoverageFrom: [
+    '**/client/**/*.js',
+  ],
+}
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+var InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
+var publicUrl = '/public';
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'index.bundle.js',
+  },
+  mode: process.env.NODE_ENV || 'development',
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+  },
+  module: {
+    rules: [
+      {
+        // this is so that we can compile any React,
+        // ES6 and above into normal ES5 syntax
+        test: /\.(js|jsx)$/,
+        // we do not want anything from node_modules to be compiled
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(css|styl)$/,
+        include: path.join(__dirname, 'src'),
+        use: [
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg|json)$/,
+        use: [
+          {
+            loader: 'file-loader', 
+          },
+          {
+            loader: 'raw-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public/index.html'),
+    }),
+    new InterpolateHtmlPlugin(HtmlWebpackPlugin, {
+      PUBLIC_URL: publicUrl,
+    }),
+  ],
+};
