2bd569d4880307948298341498da9af31a1c4904
Dean Pugh <deanpugh@deanpugh.com>
2019-06-30 21:18:58 +0100

    Add emotionJS styling to Navigation Component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,21 @@
-import React from 'react';
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
 import PropTypes from 'prop-types';
 import NavigationLink from '../NavigationLink/NavigationLink';
+
+const style = css`
+  color: hotpink;
+  display: flex;
+  list-style: none;
+  margin-top: 0;
+  padding-left: 0;
+`;
 
 export const Navigation = (props = {}) => {
   const { links = [] } = props;
   
   return (
-    <ul>
+    <ul css={style}>
     {links.map((link, index) => 
       <NavigationLink 
         key={index} 
