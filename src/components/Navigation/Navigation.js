5709da9623789826f92d6265eba2e690153bb831
Dean Pugh <deanpugh@deanpugh.com>
2019-07-01 07:42:51 +0100

    Alter navigation style

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,25 +4,31 @@ import PropTypes from 'prop-types';
 import NavigationLink from '../NavigationLink/NavigationLink';
 
 const style = css`
-  color: hotpink;
-  display: flex;
-  list-style: none;
   margin-top: 0;
   padding-left: 0;
+  background-color: #3939a0;
+
+  ul {
+    list-style: none;
+    display: flex;
+    padding: 0;
+  }
 `;
 
 export const Navigation = (props = {}) => {
   const { links = [] } = props;
   
   return (
-    <ul css={style}>
-    {links.map((link, index) => 
-      <NavigationLink 
-        key={index} 
-        label={link.label} 
-        url={link.url} />
-    )}
-    </ul>
+    <div css={style}>
+      <ul>
+      {links.map((link, index) => 
+        <NavigationLink 
+          key={index} 
+          label={link.label} 
+          url={link.url} />
+      )}
+      </ul>
+    </div>
   );
 };
 
