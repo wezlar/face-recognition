5b218e5b991e9d9f585121488c5f87e4d7bfdc39
Dean Pugh <deanpugh@deanpugh.com>
2019-09-12 08:17:08 +0100

    Add beginings of index test

---
+++ src/index.test.js
@@ -0,0 +1,17 @@
+import {
+  render,
+  cleanup,
+  waitForElement,
+} from '@testing-library/react';
+
+// import index from './index';
+
+afterEach(cleanup);
+
+describe(`Test ImageInput`, () => {
+  test.skip(`Test snapshot`, async () => {
+    const { container, debug } = render(index);
+    debug()
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
